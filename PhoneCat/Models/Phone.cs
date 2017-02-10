0e2686e4d9411a54504d6374da3472d767e0f8ed
ilya.maksimenko <ilya.maksimenko@ESW350>
2017-02-10 09:29:04 +0300

    Created Model, Context

---
+++ PhoneCat/Models/Phone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneCat.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Age { get; set; }
+        public string Snippet { get; set; }
+    }
+}
