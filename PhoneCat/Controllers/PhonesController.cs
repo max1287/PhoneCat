0ff08a143dfab612ca3f8f0f303e5ea1444b2831
ilya.maksimenko <ilya.maksimenko@ESW350>
2017-02-10 09:56:58 +0300

    Add PhonesController with GET, add using camel case

---
+++ PhoneCat/Controllers/PhonesController.cs
@@ -0,0 +1,39 @@
+﻿using PhoneCat.DAL;
+using PhoneCat.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PhoneCat.Controllers
+{
+    public class PhonesController : ApiController
+    {
+        private PhoneCatContext db = new PhoneCatContext();
+
+        //GET api/phones
+        public IQueryable<PhoneDTO> GetPhones()
+        {
+            var phones = from p in db.Phones
+                         select new PhoneDTO()
+                         {
+                             Id = p.Id,
+                             Name = p.Name,
+                             Age = p.Age,
+                             Snippet = p.Snippet
+                         };
+            return phones;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
